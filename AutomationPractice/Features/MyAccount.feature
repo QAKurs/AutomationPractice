decc7821147785e01343bf7d27db9fe0134fd8ed
Manic, Marko (GOWI) <marko.manic@euromoneyplc.com>
2018-12-08 00:59:55 +0100

    Added test for user login

---
+++ AutomationPractice/Features/MyAccount.feature
@@ -0,0 +1,11 @@
+﻿Feature: MyAccount
+	In order to use all functionalities 
+	As a user
+	I want to be able to manage my account
+
+@MyAccount
+Scenario: User can log in
+	Given user opens sign in page
+	And enters correct credentials
+	When user submits the login form
+	Then user will be logged in
---
+++ AutomationPractice/Steps/MyAccountSteps.cs
@@ -0,0 +1,45 @@
+﻿using AutomationPractice.Helpers;
+using AutomationPractice.Pages;
+using NUnit.Framework;
+using System;
+using TechTalk.SpecFlow;
+
+namespace AutomationPractice.Steps
+{
+    [Binding]
+    public class MyAccountSteps : Base
+    {
+        [Given(@"user opens sign in page")]
+        public void GivenUserOpensSignInPage()
+        {
+            Utilities ut = new Utilities(Driver);
+            HomePage hp = new HomePage(Driver);
+            ut.ClickOnElement(hp.signIn);
+        }
+        
+        [Given(@"enters correct credentials")]
+        public void GivenEntersCorrectCredentials()
+        {
+            Utilities ut = new Utilities(Driver);
+            AuthenticationPage ap = new AuthenticationPage(Driver);
+            ut.EnterTextInElement(ap.username, TestConstants.Username);
+            ut.EnterTextInElement(ap.password, TestConstants.Password);
+        }
+        
+        [When(@"user submits the login form")]
+        public void WhenUserSubmitsTheLoginForm()
+        {
+            Utilities ut = new Utilities(Driver);
+            AuthenticationPage ap = new AuthenticationPage(Driver);
+            ut.ClickOnElement(ap.signInBtn);
+        }
+        
+        [Then(@"user will be logged in")]
+        public void ThenUserWillBeLoggedIn()
+        {
+            Utilities ut = new Utilities(Driver);
+            MyAccount ma = new MyAccount(Driver);
+            Assert.True(ut.ElementDisplayed(ma.logOut), "User is not logged in");
+        }
+    }
+}
